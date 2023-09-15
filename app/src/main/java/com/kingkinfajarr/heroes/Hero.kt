8fbc5b48c1d32900291fff6222759c8d83cc6fb6
Kingkin Fajar Anifianto <kingkinfajar8@gmail.com>
2023-09-15 14:16:22 +0700

    chore: init project

---
+++ app/src/main/java/com/kingkinfajarr/heroes/Hero.kt
@@ -0,0 +1,11 @@
+package com.kingkinfajarr.heroes
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int,
+) : Parcelable
